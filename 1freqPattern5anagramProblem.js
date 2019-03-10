210b0bbdee7aa18974a20c7b0646b9ee8b2a9b24
April Copes <antdevelopment1@gmail.com>
2019-03-09 23:12:25 -0500

    generic anagram

---
+++ 1freqPattern5anagramProblem.js
@@ -0,0 +1,10 @@
+// ======================================
+// Anagram Solution O(N) Time Complexity
+// ======================================
+// Create a function that takes two strings and checks if they are valid anagrams
+
+// Examples
+// "pie", "eip" = true
+// "pie", "pie" = true
+// "pie", "pies" = false
+// "pie", "pwe" = false

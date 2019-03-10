fd2723e46180aadf539a6308700b65dda94f0cc3
April Copes <antdevelopment1@gmail.com>
2019-03-09 23:10:30 -0500

    hacker rank pseudo code anagram

---
+++ 1freqPattern8anagramHackerRankPseudoCode.js
@@ -0,0 +1,47 @@
+// Create a function called anagram that expects a string
+
+    // Split the incoming string into an array
+    
+    // Save the middle value as the array length divived by 2 giving us the approximate middle of the array
+    
+    // We slice everything from the beginging of the array up until the value of the middle variable but not
+    // including the middle value
+    
+    // We slice everything from the middle to the end of the array and save it to our second variable
+    
+    // We create a count and set it equal to 0
+
+    // // Checks if the length of the string even or odd
+  
+        // If it is an odd length we return -1
+ 
+
+    // Create an empty object
+
+    // // Loops through the first word to check if each character is in the object
+    
+        // If it is we increment it plus 1
+        
+        // Otherwise we set it equal to 1
+       
+
+    // // Loops through the second word in the string and checks if that is in the string. If it is we subtract 1
+    // // If it isnt we we do nothing only because of the nature of this problem. For a traditional anagram solution
+    // // We would return false because not find the letter in the first world mean the two words dont match and are not anagrams
+    // // of one another. But this problem asks us to return the value of changed letters needed to make the two words 
+    // // anagrams of one another.
+    
+        // If we cannot find that letter in our object we do nothing
+       
+        // If we can find the letter in our object we will subtract 1 from the object
+           
+
+    // // We loop through the object after we decrement from it the found letter. The remain vlaues of the keys
+    // // are the number of changes that will need to be made and we accumulate the total into a varable and return the value
+    
+        // Accumalate all the keys values into count since this number should represent the difference
+        // in characters in our first string vs our second string and store the sum in count
+        
+   
+    // // We return the count for the number of different characters that will be needed to get them to anagrams of each other
+   

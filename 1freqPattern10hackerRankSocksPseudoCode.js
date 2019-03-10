f41031f7000dca3b3165c8bb1571965bdf04a85f
April Copes <antdevelopment1@gmail.com>
2019-03-09 23:06:40 -0500

    hacker rank socks challenge

---
+++ 1freqPattern10hackerRankSocksPseudoCode.js
@@ -0,0 +1,32 @@
+// ========================
+// Time Complexity O(N)
+// ========================
+// Create a function that accepts a number and an array
+
+    // Create an emppty object
+
+    // Let the total number of pairs equal 0
+    
+
+    
+    // Loop through array
+
+        // Check if the current thing we looking is already inside the dictionary
+        
+
+            // If it isnt set it 0 and then increment it by 1
+            
+
+    // Loop throught the dictionary
+   
+        // Check if the value of any key is even
+     
+            // We divivde the keys value by 2 and multiply by 1 and add the value to pairs
+           
+            // Otherwise if its odd number
+       
+            // We subtract 1 from the keys value then divide it by 2 and multiply it by 1 and increment pairs of socks by that value
+           
+
+    // We return the pairs of socks
+   

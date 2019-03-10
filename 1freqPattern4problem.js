fb1dfe12dfdd47b03984067be64c30aa88225c16
April Copes <antdevelopment1@gmail.com>
2019-03-09 23:17:33 -0500

    adds problem 4 freq pattern

---
+++ 1freqPattern4problem.js
@@ -0,0 +1,8 @@
+// Given two positive integers write a function to find out if two numbers have the same frequency of digits
+// The solution must have the folowing complexities - O(N) Linear
+
+// Examples
+// 123, 123 = true
+// 123, 221 = false
+// 123, 231 = true
+// 123, 1234 = false;

40fa7402a982aa989c4dc4537bf60fbffe9a43df
April Copes <antdevelopment1@gmail.com>
2019-03-09 23:07:38 -0500

    max char pseudo code

---
+++ 1freqPattern9maxCharPseudoCode.js
@@ -0,0 +1,35 @@
+// ==============================
+// Max Char O(N) Time Complexity
+// ==============================
+
+// Given a string, return the character that is most commonly used in the string.
+
+
+    // Create an empty object
+
+    // Create a temp variable
+
+    // Create a current letter variable
+
+
+    // Loop of the string
+   
+        // Save the first character of the first string to a variable
+       
+        // Check if the current value is inside the object
+     
+            // If it is add 1
+           
+            // Else set it equal to 1
+            
+
+    // Loop through the object
+    
+        // Check if the current value we are looping over is greater than out temp value
+      
+            // If it is then reassign the the temp variable to the current value
+            
+            // And set the current letter equal to the key we are looping over
+            
+    // If everything went ok we return the current letter
+   

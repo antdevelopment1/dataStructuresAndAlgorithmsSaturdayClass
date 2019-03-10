4d10f7709794f92df13af419e5e7d14c5786d758
April Copes <antdevelopment1@gmail.com>
2019-03-09 23:15:19 -0500

    anagram problem

---
+++ 1freqPattern6anagramIntProblem.js
@@ -0,0 +1,8 @@
+// Given two positive integers write a function to find out if two numbers have the same frequency of digits
+// The solution must have the folowing complexities - Time O(N)
+
+// Examples
+// 123, 123 = true
+// 123, 231 = true
+// 123, 1234 = false
+// 123, 453 = false

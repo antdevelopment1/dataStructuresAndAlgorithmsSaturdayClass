ef9992fcaa14533720572f8502e230a25d488e8f
April Copes <antdevelopment1@gmail.com>
2019-03-09 23:08:08 -0500

    max char problem

---
+++ 1freqPattern9maxCharProblem.js
@@ -0,0 +1,5 @@
+// ==============================
+// Max Char O(N) Time Complexity
+// ==============================
+
+// Given a string, return the character that is most commonly used in the string.

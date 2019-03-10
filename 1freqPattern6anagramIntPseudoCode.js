191dafe4fe5665f5d8e6df51939f502c8213048a
April Copes <antdevelopment1@gmail.com>
2019-03-09 23:14:57 -0500

    anagram integer pseudo code

---
+++ 1freqPattern6anagramIntPseudoCode.js
@@ -0,0 +1,37 @@
+// Given two positive integers write a function to find out if two numbers have the same frequency of digits
+// The solution must have the folowing complexities - Time O(N)
+
+    // Convert numbers to strings because we cannot check the length of an integer so we have to convert it
+    // to a string
+    
+
+    // Check if the length of our strings that are integers are equal to one another
+    
+        // If the lengths are not equal to each other return false
+ 
+    
+    // Create an empty object 
+    
+    // Create a for loop that loops through the first number
+  
+        // Save that current number value in a varaible
+
+        // We check is we can find that value inside our object
+  
+            // If we do find it we will increment it by 1
+           
+            // Otherwise set it equal to 1 if it isnt already inside our object
+       
+    // Loop through our second string
+
+        // Save that current number value in a varaible
+       
+        // We check is we can find that value inside our object
+
+            // If we cannot find that value in our current object we will just return false
+     
+            // Otherwise if we can find the letter
+    
+    // If everything we ok we return true
+        
+            

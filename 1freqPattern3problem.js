ec6c38afccd65b32bacb8dbae21864320ade51dc
April Copes <antdevelopment1@gmail.com>
2019-03-09 23:14:31 -0500

    freq pattern problem

---
+++ 1freqPattern3problem.js
@@ -0,0 +1,10 @@
+// Given two arrays write a function to find out if the values in the first array contain that value to the 
+// second power in the second array
+// Must be solved with an O(N) Time Complexity or Better
+
+// Examples
+// [1,2,3,4], [1,2,3,14] = true
+// [1,2,3,4], [1,4,5,6] = false
+// [1,2,3,4],[1,4,4,2] = false
+// [1,2,3,4],[1,4,3,2] = true
+// [1,2,3,4,5],[1,2,3,4] = false;

fb63b306681c3307d0ee29cdd534273f3ff21605
April Copes <antdevelopment1@gmail.com>
2019-03-15 14:58:49 -0400

    algorithm

---
+++ 1freqPattern1problem.js
@@ -0,0 +1,11 @@
+// Quadratic Solution O(N*2)
+// Given two arrays write a function to find out if two arrays have the 
+// same frequency of digits
+
+// Examples
+// [1,2,3,4], [1,2,3,4] = true
+// [1,2,3,4], [1,4,5,6] = false
+// [1,2,3,4],[1,4,4,2] = false
+// [1,2,3,4],[1,4,3,2] = true
+// [1,2,3,4,5],[1,2,3,4] = false;
+

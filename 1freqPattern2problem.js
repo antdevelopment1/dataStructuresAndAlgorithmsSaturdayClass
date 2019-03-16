1ad38edda831593850b24216d467502696910917
April Copes <antdevelopment1@gmail.com>
2019-03-15 21:18:09 -0400

    adds freq psttern rpoblem

---
+++ 1freqPattern2problem.js
@@ -0,0 +1,12 @@
+// ============================
+// Quadratic Pseduo Code O(N*2)
+// ============================
+// Given two arrays write a function to find out if the values in the first array contain that value to the 
+// second power in the second array
+
+// Examples
+// [[1,2,3,4], [1,4,9,16]] = true
+// [[1,2,3,4], [1,4,5,6]] = false
+// [[1,2,3,4],[1,4,4,16,9]] = false
+// [[1,2,3,4],[1,16,9,4]] = true
+// [[1,2,3,4,5],[1,4,9,16]] = false;

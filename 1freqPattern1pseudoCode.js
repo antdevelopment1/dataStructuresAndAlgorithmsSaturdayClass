9e7620709b9cbd7c4133607b49e92f18b5c85eb8
April Copes <antdevelopment1@gmail.com>
2019-03-15 21:17:23 -0400

    freq pattern pseudo code

---
+++ 1freqPattern1pseudoCode.js
@@ -0,0 +1,22 @@
+// Quadratic Solution O(N*2)
+// Given two arrays write a function to find out if two arrays have the same frequency of digits
+
+// Create a function that accepts two arrays
+
+    // Check if the length of two arrays are equal to each other
+   
+        // If not return false
+       
+     
+    // Loop through the first array
+    
+        // Create a variable that will hold the value of the current value we are looping through and attempt
+        // to find the index of that value in the second array
+      
+        // If we find the value we will return index otherwise we will return -1
+       
+            // If we cannot find the value in the second array return false
+            
+        // Remove the first element from the array we are looping over
+        
+    // If everything went ok return true
